a2e7b9232b24fa41da34486e60dd5235b40e99cd
Florian <florian@technotro.com>
2023-07-30 09:18:20 +0200

    d

---
+++ assets/resources.27aa8284.js
@@ -0,0 +1,1 @@
+import{d as o,u as t,b as n,o as s,e as a}from"./vendor.ba0819c9.js";var e={};const r=o({setup(c){return t(),n(),(u,p)=>(s(),a("div"))}});typeof e=="function"&&e(r);export{r as default};
